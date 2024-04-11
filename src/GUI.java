dce06a0b33e3a6e3d3f2f4b40933759037b95d69
exteriorduke319 <yarnevictanner@gmail.com>
2024-04-11 09:20:39 -0400

    simple gooey

--- src/GUI.java
+++ src/GUI.java
@@ -4,19 +4,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI {
+    private JLabel value;
+    private JLabel changeCounter;
+    private int x;
+    private int changeNum;
     public GUI(){
-        JFrame frame = new JFrame("lebron jame hairline");
-        frame.setSize(300, 500);
+        JFrame frame = new JFrame("Simple GUI");
+        frame.setSize(600, 400);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
-        panel.setLayout(new GridLayout(4,1,10,10));
+        panel.setLayout(new GridLayout(2,2,10,10));
 
-        JButton button = new JButton("nicki minaj silicone");
-        JButton button2 = new JButton("rick ross strechmark");
-        JButton button3 = new JButton("shaq readinbook");
-        JButton button4 = new JButton(new ImageIcon("/Users/tanneryarnevic/Desktop/IntelliJ projects/guipractice/src/images.png"));
+        x = 0;
+        changeNum = 0;
+        value = new JLabel("Value: " + x);
+        value.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(value);
+
+        changeCounter = new JLabel("Number of Changes: " + x);
+        changeCounter.setHorizontalAlignment(JLabel.CENTER);
+        panel.add(changeCounter);
+
+        JButton button = new JButton("-1");
+        JButton button2 = new JButton("+1");
+
 
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -30,26 +43,8 @@ public class GUI {
             }
         });
 
-        button3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                b3Press();
-            }
-        });
-
-        button4.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                b4Press();
-            }
-        });
-
         panel.add(button);
         panel.add(button2);
-        panel.add(button3);
-        panel.add(button4);
-
-        label = new JLabel("shaq readinbook");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        panel.add(label);
 
         frame.add(panel);
         frame.setVisible(true);
@@ -60,6 +55,18 @@ public class GUI {
     }
 
     public void b1Press() {
-        label.setText("You clicked nicki minaj silicone");
+        x -= 1;
+        afterPress();
+    }
+
+    public void b2Press() {
+        x += 1;
+        afterPress();
+    }
+
+    public void afterPress() {
+        changeNum += 1;
+        value.setText("Value: " + x);
+        changeCounter.setText("Number of Changes: " + changeNum);
     }
 }
